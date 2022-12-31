7276f38e455ab7b5abcd5e4f5a79603c2c20ecc8
daiki-tadokoro <>
2022-12-31 11:30:47 +0900

    feat: transition set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
-import { AutoBatchOther } from "./components/AutoBatchOther";
+import { Transition } from "./components/Transition";
+// import { AutoBatchEventHandler } from "./components/AutoBatchEventHandler";
+// import { AutoBatchOther } from "./components/AutoBatchOther";
 
 function App() {
   return (
     <div className="App">
-      <AutoBatchEventHandler />
-      <AutoBatchOther />
+      {/* <AutoBatchEventHandler />
+      <AutoBatchOther /> */}
+      <Transition />
     </div>
   );
 }
---
+++ src/components/Transition.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+
+type Task = {
+  id: number;
+  title: string;
+  assingnee: string;
+};
+
+const member = {
+  a: "A",
+  b: "B",
+  c: "C",
+};
+
+const generateDummyTasks = (): Task[] => {
+  return Array(10000)
+    .fill("")
+    .map((_, index) => {
+      const addedIndex = index + 1;
+      return {
+        id: addedIndex,
+        title: `タスク${addedIndex}`,
+        assingnee:
+          addedIndex % 3 === 0
+            ? member.a
+            : addedIndex % 2 === 0
+            ? member.b
+            : member.c,
+      };
+    });
+};
+
+const tasks = generateDummyTasks();
+
+const filteringAssignee = (assignee: string) => {
+  if (assignee === "") return tasks;
+  return tasks.filter((task) => task.assingnee === assignee);
+};
+
+export const Transition = () => {
+  const [selectedAssingnee, setSelectedAssingnee] = useState<string>("");
+  const [taskList, setTaskList] = useState<Task[]>(tasks);
+
+  const onClickAssignee = (assignee: string) => {
+    setSelectedAssingnee(assignee);
+    setTaskList(filteringAssignee(assignee));
+  };
+
+  return (
+    <div>
+      <p>transition</p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Avatar
+          children={member.a}
+          isSelected={selectedAssingnee === member.a}
+          onClick={onClickAssignee}
+        />
+        <Avatar
+          children={member.b}
+          isSelected={selectedAssingnee === member.b}
+          onClick={onClickAssignee}
+        />
+        <Avatar
+          children={member.c}
+          isSelected={selectedAssingnee === member.c}
+          onClick={onClickAssignee}
+        />
+      </div>
+      <br />
+      <button
+        onClick={() => {
+          onClickAssignee("");
+        }}
+      >
+        リセット
+      </button>
+      {taskList.map((task) => (
+        <div
+          key={task.id}
+          style={{ width: "300px", margin: "auto", background: "lavender" }}
+        >
+          <p>タイトル：{task.title}</p>
+          <p>担当：{task.assingnee}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
